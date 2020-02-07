13186e28d1f09bfc969fde68100f4d609cb90f0c
aeshthetic <eabhijay@protonmail.com>
2020-02-07 08:35:39 -0500

    Designed minimal record types and set up Paket

---
+++ src/App/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Argu
+
+type Task = {
+    name: string;
+    dueAt: DateTime option;
+    duration: TimeSpan option;
+    dependsOn: Task list option;
+    startsAt: DateTime option;
+}
+
+type Goal = {
+    name: string;
+    tasks: Task list;
+}
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
