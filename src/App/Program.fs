55b770bca5e9d27a0457056f8dbb46b6b8892015
aeshthetic <eabhijay@protonmail.com>
2020-02-07 08:58:19 -0500

    Added argu parser skeleton

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,6 +3,67 @@
 open System
 open Argu
 
+type NewArgs =
+    | [<CliPrefix(CliPrefix.None)>] Goal of name:string
+    | [<CliPrefix(CliPrefix.None)>] Task
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Goal _ -> "Create a new goal with the specified name"
+            | Task -> "Create a new task as part of a goal"
+and UpdateArgs =
+    | [<CliPrefix(CliPrefix.None)>] Goal of name:string
+    | [<CliPrefix(CliPrefix.None)>] Task of goal:string * name:string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Goal _ -> "Update a goal with the specified name"
+            | Task _ -> "Update a task within the specified goal and of the specified name"
+and DeleteArgs =
+    | [<CliPrefix(CliPrefix.None)>] Goal of name:string
+    | [<CliPrefix(CliPrefix.None)>] Task of goal:string * name:string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Goal _ -> "Delete a goal with the specified name"
+            | Task _ -> "Delete a task within the specified goal and of the specified name"
+and DisplayArgs =
+    | [<CliPrefix(CliPrefix.None)>] Goal of name:string
+    | [<CliPrefix(CliPrefix.None)>] Task of goal:string * name:string
+    | [<CliPrefix(CliPrefix.None)>] Progress
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Goal _ -> "Display information about a goal with the specified name"
+            | Task _ -> "Display information about a task within the specified goal and of the specified name"
+            | Progress -> "Display your progress across all goals"
+and CompleteArgs =
+    | [<CliPrefix(CliPrefix.None)>] Task of goal:string * name:string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Task _ -> "Complete a task within the specified goal and of the specified name"
+and AchieveArgs =
+    | [<CliPrefix(CliPrefix.None)>] New of ParseResults<NewArgs>
+    | [<CliPrefix(CliPrefix.None)>] Update of ParseResults<UpdateArgs>
+    | [<CliPrefix(CliPrefix.None)>] Delete of ParseResults<DeleteArgs>
+    | [<CliPrefix(CliPrefix.None)>] Display of ParseResults<DisplayArgs>
+    | [<CliPrefix(CliPrefix.None)>] Complete of ParseResults<CompleteArgs>
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | New _ -> "Create a new goal or task"
+            | Update _ -> "Update an existing goal or task"
+            | Delete _ -> "Delete an existing goal or task"
+            | Display _ -> "Display information about a goal or task, or show progress across goals"
+            | Complete _ -> "Mark a task as completed"
+
 type Task = {
     name: string;
     dueAt: DateTime option;
@@ -16,7 +77,13 @@ type Goal = {
     tasks: Task list;
 }
 
+
+let parser = ArgumentParser.Create<AchieveArgs>(programName = "achieve")
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    try
+        parser.ParseCommandLine(inputs = argv, raiseOnUsage = true) |> ignore
+    with e ->
+        printfn "%s" e.Message
+    0
